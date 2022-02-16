61cbd889acec5981b45742147a76a7b5b3f60074
Patnekuv Ros (Junhyeop Lim) <limjunehyeop@gmail.com>
2022-02-16 21:56:40 +0900

    major update

--- server.js
+++ server.js
@@ -13,23 +13,31 @@ var app = http.createServer(function (request, response) {
 		return response.writeHead(404);
 	}
 	response.writeHead(200);
-	var template = `
+	fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+		var template = `
     <!doctype html>
     <html>
     <head>
-      <title>WEB1 - ${title}</title>
+      <title>${title}</title>
       <meta charset="utf-8">
     </head>
+      <style>
+        html{
+          font-family : 'Arial'
+        }
+      </style>
     <body>
-      <h1><a href="/">WEB</a></h1>
+      <h1><a href="/?id=web">WEB</a></h1>
       <ul>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
+        <li><a href="/?id=Code">Code</a></li>
+        <li><a href="/?id=Lorem">Lorem</a></li>
       </ul>
       <h2>${title}</h2>
+      <p>${description}</p>
     </body>
     </html>
     `;
-	response.end(template);
+		response.end(template);
+	});
 });
 app.listen(3000);
